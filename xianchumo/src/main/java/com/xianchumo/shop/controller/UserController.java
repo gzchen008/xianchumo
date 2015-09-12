cc6bb1ab7842ad2b40cb9241d1d26548a1613190
vanroid <793847469@qq.com>
2015-09-12 22:59:48 +0800

    usercontroller

---
+++ xianchumo/src/main/java/com/xianchumo/shop/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.xianchumo.shop.controller;
+/**
+ * 
+ * Copyright 2015 xianchumo.com
+ * @Team xianchumo
+ * @data：2015年9月12日
+ * @author CGZ
+ * @description
+ */
+public class UserController {
+
+}
